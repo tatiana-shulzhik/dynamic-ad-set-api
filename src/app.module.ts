5c99eab229db7a929c8ce2eec755908d530a9857
Татьяна <tatiana.shulzhik@gmail.com>
2025-02-20 21:17:32 +0300

    feat: настройка postgres в докер, конфигурация с бд, настройка миграций

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configTreeModule } from './config-tree/config-tree.module';
+import { typeOrmConfig } from './data-source';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    configTreeModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
